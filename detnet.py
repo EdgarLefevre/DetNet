360ce27583441e1658b53777e7f55b4820a7c694
elefevre <lefevreedg@gmail.com>
2020-03-12 14:42:17 +0100

    fix train

--- detnet.py
+++ detnet.py
@@ -58,4 +58,16 @@ def dice_coef(y_true, y_pred, smooth, thresh):
 def dice_loss(smooth, thresh):
     def dice(y_true, y_pred):
         return -dice_coef(y_true, y_pred, smooth, thresh)
+
     return dice
+
+
+def dice_coef2(y_true, y_pred, smooth=1e-6):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def soft_dice_loss(y_true, y_pred):
+    return 1 - dice_coef2(y_true, y_pred)
--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def train(path_images, path_labels):
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         model = detnet.detnet((512, 512, 1))
-        model_dice = detnet.dice_loss(smooth=1e-6, thresh=0.5)
+        model_dice = detnet.soft_dice_loss
         model.compile(loss=model_dice, optimizer=tf.keras.optimizers.Adam(lr=0.001, amsgrad=True))
         model.fit(dataset, label,
                   validation_split=0.2,
