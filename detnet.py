169df9d44c35fd42f15fde19e8a971bf42561f45
elefevre <lefevreedg@gmail.com>
2020-03-12 14:35:22 +0100

    fix train

--- detnet.py
+++ detnet.py
@@ -1,6 +1,7 @@
 import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
 import tensorflow.keras.backend as K
+import tensorflow as tf
 import numpy as np
 
 
@@ -45,8 +46,8 @@ def detnet(input_shape, kernel=3, filters=16):
 
 
 def dice_coef(y_true, y_pred, smooth, thresh):
-    y_true = y_true.astype(np.float32)
-    y_pred = y_pred.astype(np.float32)
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
     y_pred = y_pred > thresh
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
