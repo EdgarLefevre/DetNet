46f35a9d6b0b71d1889c34b922421bf185456936
elefevre <lefevreedg@gmail.com>
2020-03-12 14:51:55 +0100

    fix train

--- detnet.py
+++ detnet.py
@@ -63,8 +63,8 @@ def dice_loss(smooth, thresh):
 
 
 def dice_coef2(y_true, y_pred, smooth=1e-6):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
+    y_true_f = K.flatten(tf.cast(y_true, tf.float32))
+    y_pred_f = K.flatten(tf.cast(y_pred, tf.float32))
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
